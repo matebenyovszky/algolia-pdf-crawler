21d44ff62b26cbafc88716036bf2f1106577e9b5
matebenyovszky <57362482+matebenyovszky@users.noreply.github.com>
2023-11-15 00:04:42 +0100

    Delete algolia_integration.js

--- algolia_integration.js
+++
@@ -1,7 +0,0 @@
-function readSettingsFile() {
-  // Read the settings file and extract the Algolia API key and other necessary information
-  // Implement the logic to extract the API key and return it
-  // Example: 
-  // const algoliaApiKey = ... ;
-  // return algoliaApiKey;
-}
