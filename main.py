a855ba352edf97b8b01cc4c97bdb85e7640b3f74
Máté Benyovszky <mate.benyovszky@inhat.hu>
2023-11-27 10:00:35 +0100

    Updated main.py to include Algolia settings from .env file Added Algolia API key, App ID, and Index Name to .env file Modified pdf_scraper.py to check if PDFs are already in the database Enabled OCR for PDF files in main.py Enabled upload to Algolia in main.py

--- main.py
+++ main.py
@@ -5,16 +5,12 @@ def main():
     with open("config.json") as f:
         config = json.load(f)
 
-    pdf_scrape_enabled = config["pdf_scrape_enabled"]
-    msdiread_ocr_enabled = config["msdiread_ocr_enabled"]
-    upload_enabled = config["upload_enabled"]
-
     for website in config["websites"]:
 
-        print(f"Website URL: {website["url"]}")
-        print(f"Language: {website["language"]}")
+        print(f"Website URL: {website['url']}")
+        print(f"Language: {website['language']}")
 
-        if pdf_scrape_enabled:
+        if config["pdf_scrape_enabled"]:
             # Search PDF files
             from pdf_scraper import get_pdf_links
             pdf_links = get_pdf_links(website["url"], website["base_url"], website["skip"])
@@ -26,12 +22,12 @@ def main():
         if not pdf_links:
             continue
 
-        if msdiread_ocr_enabled:
+        if config["msdiread_ocr_enabled"]:
             # OCR PDF files
             from pdf_ocr_msdiread import run_ocr_on_pdf
             scraped_data = run_ocr_on_pdf(pdf_links, website["language"])
 
-        if upload_enabled:
+        if config["upload_enabled"]:
             # Upload data to Algolia
             from algolia import upload_to_algolia
             upload_to_algolia(scraped_data)
